ebe51470982f03bc08f2c1a45cb56a2870519314
Maximo Hinojosa <maximohinojosa13@gmail.com>
2018-11-18 13:08:46 -0800

    users can see donators

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const exphbs = require('express-handlebars');
 const app = express()
 const port = process.env.PORT || 3000;
 
@@ -9,6 +10,15 @@ app.set('view engine', '.hbs');
 
 app.get("/", (req, res) => {
     res.send('Hello World!')
+});
+
+let givers = [
+    { companyName: "Netflix", amount: "$5,000.00"},
+    { companyName: "Spodify", amount: "$2,000.00"}
+]
+
+app.get("/donators", (req, res) => {
+    res.render('donators-index', {givers: givers});
 });
 
 if (!module.parent) {
