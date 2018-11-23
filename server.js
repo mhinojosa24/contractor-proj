2df51175b72a5d7271ecd07d91e28f230e9e9672
Maximo Hinojosa <maximohinojosa13@gmail.com>
2018-11-23 15:11:59 -0500

    Added MongoDB and Donor model

--- server.js
+++ server.js
@@ -2,13 +2,15 @@ const express = require('express')
 const exphbs = require('express-handlebars');
 const app = express()
 const port = process.env.PORT || 3000;
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/Charity-Donor');
 
 
 app.engine('.hbs', exphbs({ extname: '.hbs', defaultLayout: 'main'}));
 app.set('view engine', '.hbs');
 // app.use(bodyParser.urlencoded({ extend: true}));
 
-
+const Donors = mongooe..model(Donor)
 
 
 let givers = [
