74a9af6041faba8b7669365a6ffc172f6d4aa136
Maximo Hinojosa <maximohinojosa13@gmail.com>
2018-11-26 18:47:42 -0700

    Users able to specific donators

--- controllers/donors.js
+++ controllers/donors.js
@@ -8,7 +8,7 @@ module.exports = function(app) {
     app.get("/", (req, res) => {
         Donor.find()
             .then(givers => {
-                res.render('donators-index', {givers: givers});
+                res.render('all-donators', {givers: givers});
             }).catch(err => {
                 console.log(err);
             });
@@ -23,7 +23,16 @@ module.exports = function(app) {
     app.post('/donors', (req, res) => {
         Donor.create(req.body).then((donor) => {
             console.log(donor)
-            res.redirect('/');
+            res.redirect(`/donors/${donor._id}`);
+        }).catch((err) => {
+            console.log(err.message);
+        });
+    });
+
+    // show
+    app.get('/donors/:id', (req, res) => {
+        Donor.findById(req.params.id).then((donor) => {
+            res.render('show-donators', {donor: donor});
         }).catch((err) => {
             console.log(err.message);
         });
